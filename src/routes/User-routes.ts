9867240c98026ce954e156361b1058041f20cfc8
PraveenMali9571 <praveenmali9571@gmail.com>
2026-02-25 23:19:35 +0530

    Revert "feat: creating the structure and api for create user"
    This reverts commit 4568805b74d3f7a02fc844d77460032a57f05cec.

--- src/routes/User-routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { UserCreate } from "../controllers/User-controller.ts";
-const UserRouter = express.Router();
-
-    // All the Routes
-UserRouter.post("/UserCame",UserCreate);
-
-export default UserRouter;
